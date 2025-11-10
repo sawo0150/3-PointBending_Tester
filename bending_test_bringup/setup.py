016c191b862452d6db31d7b6e7223b4c063eca04
sawo0150 <swpants05@gmail.com>
2025-11-11 00:25:40 +0900

    완성!!

--- bending_test_bringup/setup.py
+++ bending_test_bringup/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'bending_test_bringup'
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # [!!! 중요 !!!] 런치 파일을 설치하도록 이 부분을 추가/수정합니다.
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
--- experiment_manager/setup.py
+++ experiment_manager/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'experiment_manager'
 
@@ -10,6 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # [추가] 런치 파일 설치
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.launch.py'))),
+        # [추가] 파라미터(config) 파일 설치
+        (os.path.join('share', package_name, 'config'),
+            glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +32,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            # [추가] 노드 실행파일로 등록
+            'experiment_manager_node = experiment_manager.experiment_manager_node:main',
         ],
     },
 )
